b4215bf6a530fc31a19866f4f9583e1b1aa52b4e
manippoudel <manippoudel@gmail.com>
2023-01-26 21:39:59 +0545

    update: load balancer and security group module in main

--- dev/main.tf
+++ dev/main.tf
@@ -6,4 +6,28 @@ module "vpc" {
   public_subnet_cidr_blocks  = var.public_subnet_cidr_blocks
   private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
   availability_zones         = var.availability_zones
+  region                     = var.region
+}
+
+module "security_group" {
+  source       = "./modules/security_group"
+  env          = var.env
+  project_name = var.project_name
+  vpc_id       = module.vpc.vpc_id
+  depends_on = [
+    module.vpc
+  ]
+}
+
+module "load_balancer" {
+  source                    = "./modules/load_balancer"
+  env                       = var.env
+  project_name              = var.project_name
+  public_subnet_cidr_blocks = module.vpc.public_subnet_list
+  vpc_id                    = module.vpc.vpc_id
+  alb_sg                    = module.security_group.alb_sg
+  depends_on = [
+    module.security_group,
+    module.vpc
+  ]
 }
--- dev/vars.tf
+++ dev/vars.tf
@@ -1,15 +1,45 @@
-variable "region" {}
-variable "env" {
-  # type = string
-  # description = "Environment for which the infrastructure is being created"
-}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+variable "env" {
+  type        = string
+  description = "Environment for which the infrastructure is being created"
+}
+
 variable "project_name" {
   type        = string
   description = "The name of the project"
 }
-variable "vpc_cidr" {}
-variable "public_subnet_cidr_blocks" {}
-variable "private_subnet_cidr_blocks" {}
-variable "availability_zones" {}
+
+#Networking Related.
+###########################################
+variable "vpc_cidr" {
+  type        = string
+  description = "CIDR Range for our VPC"
+  default     = "10.10.0.0/16"
+}
+variable "public_subnet_cidr_blocks" {
+  default     = ["10.20.0.0/20", "10.20.16.0/20", "10.20.32.0/20"]
+  type        = list(any)
+  description = "List of public subnet CIDR blocks"
+}
+
+variable "private_subnet_cidr_blocks" {
+  default     = ["10.20.128.0/20", "10.20.144.0/20", "10.20.160.0/20"]
+  type        = list(any)
+  description = "List of private subnet CIDR blocks"
+}
+
+variable "availability_zones" {
+  default     = ["ap-northeast-1a", "ap-northeast-1c", "ap-northeast-1d"]
+  type        = list(any)
+  description = "List of availability zones"
+}
+
+variable "region" {
+  type        = string
+  description = "Region of the resources to be created."
+  default     = "ap-northeast-1"
+}
+
+##########################################################
+
