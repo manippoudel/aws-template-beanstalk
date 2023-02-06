72cee12e8c6dca349584533fb0459b676507ef4c
manippoudel <manippoudel@gmail.com>
2023-02-06 14:55:36 +0545

    fix: rules for the elastic beanstalk

--- dev/modules/load_balancer/main.tf
+++ dev/modules/load_balancer/main.tf
@@ -9,3 +9,59 @@ resource "aws_lb" "default" {
   }
 }
 
+
+resource "aws_lb_target_group" "shared_target_group" {
+  name     = "${var.project_name}-${var.env}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_target_group" "shared_target_group_https" {
+  name     = "${var.project_name}-${var.env}-https-tg"
+  port     = 443
+  protocol = "HTTPS"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_listener" "shared_lb_listener_1" {
+  load_balancer_arn = aws_lb.default.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.shared_target_group.arn
+
+  }
+
+}
+
+
+# resource "aws_elbv2_listener" "go" {
+#   load_balancer_arn = aws_lb.default.arn
+#   port              = "80"
+#   protocol          = "HTTP"
+
+#   default_action {
+#     target_group_arn = aws_lb_target_group.shared_target_group.arn
+#     type             = "forward"
+#   }
+# }
+# resource "aws_elbv2_listener_rule" "go" {
+#   listener_arn = aws_elbv2_listener.go.arn
+#   priority     = 1
+
+#   action {
+#     type = "forward"
+
+#     target_group_arn = aws_elastic_beanstalk_environment.go.endpoint.0.arn
+#   }
+
+#   condition {
+#     path_pattern {
+#       values = ["/go/*", "/go"]
+#     }
+#   }
+# }
